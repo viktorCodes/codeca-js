d7357c19042659dec88a5a7877d99b95b950bfa1
calimendoza <mbaivictor1@gmail.com>
2022-07-26 21:05:22 -0700

    added get user choice

--- rock-paper-scissors/rockPaperScissors.js
+++ rock-paper-scissors/rockPaperScissors.js
@@ -1,1 +1,14 @@
 /* Rock, Paper, Scissors Game */
+
+//GET USER CHOICE
+
+const getUserChoice = (userInput) => {
+    userInput = userInput.toLowerCase();
+
+    if(userInput === 'rock' || userInput === 'paper' || userInput === 'scissors'){
+        return userInput;
+    } else{
+        console.log('Error!');
+    };
+};
+console.log(getUserChoice('rock'));
