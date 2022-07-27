a72f32cfb136c3b50183c5c86057327881e5971d
calimendoza <mbaivictor1@gmail.com>
2022-07-26 21:11:33 -0700

    added get computer choice function

--- rock-paper-scissors/rockPaperScissors.js
+++ rock-paper-scissors/rockPaperScissors.js
@@ -12,3 +12,20 @@ const getUserChoice = (userInput) => {
     };
 };
 console.log(getUserChoice('rock'));
+
+//GET COMPUTER CHOICE
+
+const getComputerChoice = () => {
+
+    const randomNumber = Math.floor(Math.random() *3);
+
+    switch(randomNumber){
+        case 0:
+            return 'rock';
+            case 1: 
+            return 'paper';
+            case 2: 
+            return 'scissors';
+    };
+};
+console.log(getComputerChoice());
