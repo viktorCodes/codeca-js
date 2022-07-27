1b385724ee8cc47284a1bfefc5807562fb38c549
calimendoza <mbaivictor1@gmail.com>
2022-07-27 01:32:51 -0700

    added get sleep hours

--- sleep-debt-calculator/sleepDebtCalculator.js
+++ sleep-debt-calculator/sleepDebtCalculator.js
@@ -1,1 +1,33 @@
 /* Sleep Debt Calculator */
+
+//GET SLEEP HOURS
+
+
+const getSleepHours = (day) =>{
+
+    switch(day){
+        case 'monday':
+            return 9
+            break;
+            case 'tuesday':
+                return 8
+                break;
+                case 'wednesday':
+                    return 7
+                    break;
+                    case 'thursday':
+                        return 8
+                        break;
+                        case 'friday':
+                            return 7
+                            break;
+                            case 'saturday':
+                                return 6
+                                break;
+                                case 'sunday':
+                                    return 8
+                                    break;
+                                    default:
+                                        return 'Error!'
+    };
+};
