4dac3fa7578a65199178f538c9270540f22da6fa
calimendoza <mbaivictor1@gmail.com>
2022-07-28 23:50:39 -0700

    finished project

--- training-days/trainingDays.js
+++ training-days/trainingDays.js
@@ -47,6 +47,13 @@ logEvent(name, event);
 logTime(name, days);
 
 
+const event2 = getRandEvent();
+const days2 = getTrainingDays(event2);
+const name2 = 'Warren';
+ 
+logEvent(name2, event2);
+logTime(name2, days2);
+
 
 
 
