b73e81f3a1205ddd7ba34dfcb451aa5a2a1aacd0
calimendoza <mbaivictor1@gmail.com>
2022-07-31 23:31:18 -0700

    removed last string in array

--- secret-message/main.js
+++ secret-message/main.js
@@ -1,3 +1,7 @@
 //SECRET MESSAGE
 
 let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+//REMOVE LAST STRING OF ARRAY
+
+secretMessage.pop()
