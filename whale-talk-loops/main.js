9bd7a6f16722a17ce922b5cbbea333c25aa20a15
calimendoza <mbaivictor1@gmail.com>
2022-08-01 23:08:24 -0700

    created nested for loop

--- whale-talk-loops/main.js
+++ whale-talk-loops/main.js
@@ -11,5 +11,10 @@ const resultArray = [];
 //CREATE A LOOP THAT ITERATES THROUGH EACH LETTER OF THE INPUT VARIALBLE
 
 for(let i = 0; i < input.length; i++){
-     console.log(`input is ${i}`);
+    // console.log(`input is ${i}`);
+
+    //CREATE NESTED FOR LOOP
+    for( let v = 0; v < vowels.length; v++){
+        console.log(`vowel is ${v}`);
+    }
 }
