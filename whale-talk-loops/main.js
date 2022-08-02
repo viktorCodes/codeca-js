8ccc5e6f7e335e47eb989cf2dc87c9842acd0354
calimendoza <mbaivictor1@gmail.com>
2022-08-01 23:04:28 -0700

    added for loop

--- whale-talk-loops/main.js
+++ whale-talk-loops/main.js
@@ -6,4 +6,10 @@ const input = 'turpentine and turtles';
 
 const vowels = ['a', 'e', 'i', 'o', 'u'];
 
-const resultArray = []
+const resultArray = [];
+
+//CREATE A LOOP THAT ITERATES THROUGH EACH LETTER OF THE INPUT VARIALBLE
+
+for(let i = 0; i < input.length; i++){
+     console.log(`input is ${i}`);
+}
