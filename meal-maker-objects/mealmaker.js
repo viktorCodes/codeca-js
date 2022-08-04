89789182730fdb88a98b616b28a8e14d0ea08636
calimendoza <mbaivictor1@gmail.com>
2022-08-03 21:37:49 -0700

    added setter methods for meal and price

--- meal-maker-objects/mealmaker.js
+++ meal-maker-objects/mealmaker.js
@@ -3,9 +3,23 @@
 // CREATE MENU OBJECT
 
 const menu = {
-
+//CREATE MEAL AND PRICE PROPERTIES
     _meal: '',
     _price: 0,
 
-    
+//CREATE SETTER METHODS
+
+set meal(mealToCheck){
+    if(typeof mealToCheck === 'string'){
+        return this._meal = mealToCheck
+    }
+},
+
+set price(priceToCheck) {
+    if(typeof priceToCheck === 'number'){
+        return this._price = priceToCheck
+    }
+}
+
+
 }
