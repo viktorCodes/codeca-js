9560fb844cec8dd371dcfd0974f03ff0720b1392
calimendoza <mbaivictor1@gmail.com>
2022-08-03 23:26:16 -0700

    started populating data by adding players and games properties

--- team-stats-objects/main.js
+++ team-stats-objects/main.js
@@ -1,1 +1,17 @@
 // CREATE, RETRIEVE AND ADD INFORMATION ABT FAV SPORTS TEAM
+
+//POPULATING DATA
+
+const team = {
+//CREATE PLAYER AND GAME PROPERTIES
+    _players:[
+        {firstName: 'Mohammed', lastName: 'Salah', age: 30},
+        {firstName: 'Virgil', lastName: 'Van Dijk', age: 31},
+        {firstName: 'Thiago', lastName: 'Alcantara', age: 32 }
+    ],
+    _games: [
+        {opponent: 'Manchester City', teamPoints: 92, opponentPoints: 93},
+        {opponent: 'Manchester United', teamPoints: 92, opponentPoints: 67 },
+        {opponent: 'Everton', teamPoints: 92, opponentPoints: 38  }
+    ],
+}
