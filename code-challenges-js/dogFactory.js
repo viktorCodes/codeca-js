6a7a85851f18ddd10426ae40cacff4a6f3fced20
calimendoza <mbaivictor1@gmail.com>
2022-08-07 07:05:47 -0700

    pt 1

---
+++ code-challenges-js/dogFactory.js
@@ -0,0 +1,19 @@
+/*Write a function, dogFactory(). It should:
+
+have 3 parameters: name, breed, and weight (in that order)
+expect name and breed to be strings
+expect weight to be a number
+return an object
+have each of those parameters as keys on the returned object returned with the values of the arguments that were passed in
+dogFactory('Joe', 'Pug', 27)
+// Should return { name: 'Joe', breed: 'Pug', weight: 27 } */
+
+
+const dogFactory = (name, breed, weight) => {
+    return {
+        _name: name,
+        _breed: breed,
+        _weight: weight,
+        get name() {
+            return this._name;
+        },
