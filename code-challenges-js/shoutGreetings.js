b6ca8b70a63425e17b3b2b98eeec993375b0a866
calimendoza <mbaivictor1@gmail.com>
2022-08-07 06:10:28 -0700

    shout greetings

---
+++ code-challenges-js/shoutGreetings.js
@@ -0,0 +1,7 @@
+/*Write a function shoutGreetings() that takes in an array of strings and returns a new array. This new array should contain all the strings from the argument array but with capitalized letters and an exclamation point appended to the end: 'heya' will become 'HEYA!'
+
+You can use any technique you want to accomplish this task.
+
+You can test your function when you’re ready by passing in the greetings array or by making your own array! */
+
+const shoutGreetings = arr => arr.map(word => word.toUpperCase() + '!')
