8be82e25202f4acca5886fd38912129756097b8a
calimendoza <mbaivictor1@gmail.com>
2022-08-12 21:16:39 -0700

    create factory

--- mysterious-organism/main.js
+++ mysterious-organism/main.js
@@ -4,3 +4,26 @@ const returnRandBase = () => {
     const dnaBases = ['A', 'T', 'C', 'G'];
     return dnaBases[Math.floor(Math.random() * 4)]
 }
+
+//RETURNS A RANDOM SINGLE STRAND OF DNA CONTAINING 15 BASES
+
+const mockUpStrand = () => {
+    const newStrand = [];
+    for(let i = 0; i < 15; i++){
+        newStrand.push(returnRandBase())
+    }
+    return newStrand
+}
+
+//CREATE FACTORY
+
+const pAequorFactory = (specimanNum, dna) => {
+
+    return{
+        specimanNum,
+        dna,
+        //mutate function
+        mutate(){}
+    }
+
+}
