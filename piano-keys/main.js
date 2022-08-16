86c2b373c01445465ff5d52b793b46dfd67190ff
calimendoza <mbaivictor1@gmail.com>
2022-08-15 23:56:27 -0700

    added keys

--- piano-keys/main.js
+++ piano-keys/main.js
@@ -1,1 +1,2 @@
 // The keys and notes variables store the piano keys
+const keys = ['c-key', 'd-key', 'e-key', 'f-key', 'g-key', 'a-key', 'b-key', 'high-c-key', 'c-sharp-key', 'd-sharp-key', 'f-sharp-key', 'g-sharp-key', 'a-sharp-key'];
