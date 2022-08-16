b54105fb02b4327d0072a7ed229a3a0d7e5ac119
calimendoza <mbaivictor1@gmail.com>
2022-08-16 02:06:05 -0700

    event handler property

--- piano-keys/main.js
+++ piano-keys/main.js
@@ -90,4 +90,22 @@ nextThree.Onclick = function(){
   lastLyric.style.display = 'none'
   };
 
+  // This is the event handler property and function for the startOver button
+startOver.onclick = function() {
+    nextOne.hidden = false;
+    startOver.hidden = true;
+     document.getElementById('word-one').innerHTML = 'HAP-';
+    document.getElementById('letter-note-one').innerHTML = 'G';
+    document.getElementById('word-two').innerHTML = 'PY';
+    document.getElementById('letter-note-two').innerHTML = 'G';
+    document.getElementById('word-three').innerHTML = 'BIRTH-';
+    document.getElementById('letter-note-three').innerHTML = 'A';
+    document.getElementById('word-four').innerHTML = 'DAY';
+    document.getElementById('letter-note-four').innerHTML = 'G';
+    document.getElementById('word-five').innerHTML = 'TO';
+    document.getElementById('letter-note-five').innerHTML = 'C';
+    document.getElementById('word-six').innerHTML = 'YOU!';
+    document.getElementById('letter-note-six').innerHTML = 'B';
+  }
+
   
