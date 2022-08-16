80017b9e0dbc93980948863507f4ac4b4e3a3ccc
calimendoza <mbaivictor1@gmail.com>
2022-08-16 02:00:10 -0700

    anonymous event handler

--- piano-keys/main.js
+++ piano-keys/main.js
@@ -51,4 +51,21 @@ nextOne.onclick = function (){
     document.getElementById('letter-note-five').innerHTML = 'D';
      document.getElementById('letter-note-six').innerHTML = 'C'
   } 
+
+  // Write anonymous event handler property and function for the second progress button
+nextTwo.onclick = function(){
+    nextThree.hidden = false;
+    nextTwo.hidden = true;
+  
+     document.getElementById('word-five').innerHTML = 'DEAR';
+      document.getElementById('word-six').innerHTML = 'FRI';
+  
+       document.getElementById('letter-note-three').innerHTML = 'G'
+        document.getElementById('letter-note-four').innerHTML = 'E';
+         document.getElementById('letter-note-five').innerHTML = 'C';
+          document.getElementById('letter-note-six').innerHTML = 'B'
+  
+      lastLyric.style.display = 'inline-block'
+  };
+  
   
